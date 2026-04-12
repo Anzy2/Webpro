1c22d8383a838cfd0b44db1a47ecd576d748fff0
Anzy2 <titaniofrancynadiansah@gmail.com>
2026-04-12 14:06:50 +0700

    mengisi file portfolio-tfn

--- portfolio-laravel/resources/views/partials/footer.blade.php
+++
@@ -1,7 +0,0 @@
-<footer class="footer">
-  <div class="container footer-inner">
-    <span class="footer-logo">TFN<span>.</span></span>
-    <p>© {{ date('Y') }} {{ $owner['name'] }}. Dibuat dengan ❤️ menggunakan Laravel & CSS.</p>
-    <a href="#hero" class="back-top"><i data-feather="arrow-up"></i></a>
-  </div>
-</footer>
